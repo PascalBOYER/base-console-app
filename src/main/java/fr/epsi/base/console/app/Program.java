530978b0e348e9623882af8098de6af201b18034
Pascal BOYER <pa.boyer@free.fr>
2022-02-01 13:56:30 +0100

    Changed Hello to Bonjour

--- src/main/java/fr/epsi/base/console/app/Program.java
+++ src/main/java/fr/epsi/base/console/app/Program.java
@@ -3,7 +3,7 @@ package fr.epsi.base.console.app;
 public class Program {
 
     public static void main (String[] args) {
-        System.out.println("Hello !");
+        System.out.println("Bonjour  !");
     } 
     
 }
